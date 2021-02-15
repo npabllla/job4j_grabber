626591e79ba16254ce2189083f5a1d93e1e5adbc
Kirill <npabllla@gmail.com>
2021-02-16 00:53:44 +0300

    2.1. Преобразование даты [#451156]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,23 +4,32 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        for (Element td : row) {
-            Element href = td.child(0);
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-        }
-        Elements rw = doc.select(".altCol");
-        for (int i = 1; i < rw.size(); i++) {
-            if (i % 2 == 1) {
-                Element temp = rw.get(i);
-                temp.attr("class");
-                System.out.println(temp.text());
+            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/").get();
+            Elements row = doc.select(".postslisttopic");
+            for (Element td : row) {
+                Element href = td.child(0);
+                System.out.println(href.attr("href"));
+                System.out.println(href.text());
             }
-        }
+            Elements rw = doc.select(".altCol");
+            for (int i = 1; i < rw.size(); i++) {
+                if (i % 2 == 1) {
+                    Element temp = rw.get(i);
+                    temp.attr("class");
+                    System.out.println(transformToDate(temp.text()));
+                }
+            }
+    }
+
+    private static Date transformToDate(String date) throws ParseException {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMM yy, HH:mm", new Locale("ru"));
+        return dateFormat.parse(date);
     }
 }
