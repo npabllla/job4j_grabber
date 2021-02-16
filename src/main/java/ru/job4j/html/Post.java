317bc424039116d6e6e9d621ebbeeafe2abf00ea
Kirill <npabllla@gmail.com>
2021-02-16 16:43:43 +0300

    2.2. Модель данных - Post. [#451153]

---
+++ src/main/java/ru/job4j/html/Post.java
@@ -0,0 +1,55 @@
+package ru.job4j.html;
+
+import java.util.Objects;
+
+public class Post {
+    private String link;
+    private String description;
+    private String date;
+
+    public Post(String link, String description, String date) {
+        this.link = link;
+        this.description = description;
+        this.date = date;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" + "link='"
+                + link + '\''
+                + ", description='"
+                + description + '\''
+                + ", date='"
+                + date + '\''
+                + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Post post = (Post) o;
+        return Objects.equals(link, post.link) && Objects.equals(description, post.description) && Objects.equals(date, post.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(link, description, date);
+    }
+}
